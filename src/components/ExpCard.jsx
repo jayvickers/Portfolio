77aaf21065e56d4430d311acef83182810bf747c
unknown <jacob.e.vickers@gmail.com>
2021-07-29 17:36:59 -0600

    try swap

--- src/components/ExpCard.jsx
+++ src/components/ExpCard.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 
-const ExpCard = ({ bullets, title }) => {
+const ExpCard = ({ bullets, isCurrent, title }) => {
   const cardListItemStyles = {
     display: 'grid',
     gridTemplateColumns: '10px 1fr',
@@ -17,11 +17,23 @@ const ExpCard = ({ bullets, title }) => {
     display: 'grid',
     gridRowGap: '1rem'
   }
+
+  const FEStyles = {
+    transitionDuration: '.2s',
+    transitionProperty: 'box-shadow,color',
+    boxShadow: '0 0 2px rgb(99 183 183 / 75%), inset 0 -1.375em 0 rgb(99 183 183 / 100%)',
+    padding: '0 2px 2px',
+    position: 'relative',
+    borderRadius: '2px',
+    color: 'white'
+  }
+
   let count = 0
   const updateCnt = () => { count = count + 1 }
   return (
     <div className="exp-card">
     <h3>{title}</h3>
+    {!!isCurrent && <span style={FEStyles}>Current Role</span> }
         <ul style={listStyles}>
             {!!bullets && bullets.map((bullet) =>
                 <li style={cardListItemStyles} key={count}>
@@ -37,6 +49,11 @@ const ExpCard = ({ bullets, title }) => {
 
 ExpCard.propTypes = {
   bullets: PropTypes.array,
+  isCurrent: PropTypes.bool,
   title: PropTypes.string
+}
+
+Greeting.defaultProps = {
+  isCurrent: false
 }
 export default ExpCard
